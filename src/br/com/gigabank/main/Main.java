cd2cd16d18cb8b0a771effa20adc1e17c8b33f11
Everton Silva <everton.vitordasilva.outlook.com>
2023-08-13 17:28:22 -0300

    modificando instanciação

--- src/br/com/gigabank/main/Main.java
+++ src/br/com/gigabank/main/Main.java
@@ -8,9 +8,9 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        ContaCorrente c1 = new ContaCorrente("Everton", "361.474.968-16", 10000.00, 1234, 23, "Etakarai1988");
+        ContaCorrente c1 = new ContaCorrente("Everton", "123.456.789.01", 10000.00, 1234, 23, "Etakarai1988");
         System.out.println(c1);
-        ContaPoupanca c2 = new ContaPoupanca("Everton", "361.474.968-16", 5000.00, 54321, 23, "19101977");
+        ContaPoupanca c2 = new ContaPoupanca("Everton", "123.456.789.01", 5000.00, 54321, 23, "19101977");
         c2.sacar(1000.00);
         System.out.println(c2);
     }
